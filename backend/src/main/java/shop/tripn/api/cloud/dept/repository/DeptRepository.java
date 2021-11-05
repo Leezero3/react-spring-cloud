8d9c7e1856d6aa88c954582da88a1e6aa97d9ce8
zero3ee <fuddl4525@naver.com>
2021-11-05 20:06:51 +0900

    1105

--- backend/src/main/java/shop/tripn/api/cloud/dept/repository/DeptRepository.java
+++ backend/src/main/java/shop/tripn/api/cloud/dept/repository/DeptRepository.java
@@ -1,4 +1,6 @@
 package shop.tripn.api.cloud.dept.repository;
 
-public interface DeptRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeptRepository extends JpaRepository {
 }
