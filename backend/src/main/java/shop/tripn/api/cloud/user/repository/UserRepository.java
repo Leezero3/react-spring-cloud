4c712fecd52035c257b1bd08f6ef65406d266451
zero3ee <fuddl4525@naver.com>
2021-11-05 20:08:19 +0900

    1105

--- backend/src/main/java/shop/tripn/api/cloud/user/repository/UserRepository.java
+++ backend/src/main/java/shop/tripn/api/cloud/user/repository/UserRepository.java
@@ -12,10 +12,12 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     @Transactional
     @Query(value = "select u.user_id userId, u.username username, u.password, u.name, u.email, u.reg_date regDate  " +
             "from users u where u.username=:username and u.password=:password", // 타입 = 키
             nativeQuery = true)
+
     Optional<User> login(@Param("username") String username, @Param("password") String password);
     boolean existsByUsername(@Param("username") String username);
 }
